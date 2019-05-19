eca8a7920702aa67e94cdd19b5a6ae85b1876a6f
Naxulanth <deniz@deniz.gg>
2019-05-19 17:29:57 +0300

    Updates

--- precache-manifest.c4996f0400754318e7de1a6e8c622ebd.js
+++ precache-manifest.c4996f0400754318e7de1a6e8c622ebd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "937b76afccad13c672359a54e5d61fd4",
+    "revision": "38429f9268d6c00debe38e9c6f66fada",
     "url": "/spotify-playlist-exporter/index.html"
   },
   {
-    "revision": "0bce8b64543188da0e4d",
+    "revision": "203e66c995af7f88719e",
     "url": "/spotify-playlist-exporter/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "7a9d653d81dc921aa26b",
+    "revision": "805d3f415fc493b16bac",
     "url": "/spotify-playlist-exporter/static/css/main.75b1aa25.chunk.css"
   },
   {
-    "revision": "0bce8b64543188da0e4d",
-    "url": "/spotify-playlist-exporter/static/js/2.8df938cf.chunk.js"
+    "revision": "203e66c995af7f88719e",
+    "url": "/spotify-playlist-exporter/static/js/2.bfecffb0.chunk.js"
   },
   {
-    "revision": "7a9d653d81dc921aa26b",
-    "url": "/spotify-playlist-exporter/static/js/main.657bf6d2.chunk.js"
+    "revision": "805d3f415fc493b16bac",
+    "url": "/spotify-playlist-exporter/static/js/main.464bb9ac.chunk.js"
   },
   {
     "revision": "ce85774247f9818ec1b2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spotify-playlist-exporter/precache-manifest.61c345fc22962608f508fa3b1c230064.js"
+  "/spotify-playlist-exporter/precache-manifest.c4996f0400754318e7de1a6e8c622ebd.js"
 );
 
 self.addEventListener('message', (event) => {
