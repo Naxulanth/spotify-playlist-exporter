2c28b61650fc9aba430e2bd3fd2eefd1ef556dff
Naxulanth <deniz@deniz.gg>
2019-05-19 16:23:17 +0300

    Updates

---
+++ precache-manifest.ef76178f95af8c7fcb0662bf30281d46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59105e4e8388fdb27379aa1c3bce79a1",
+    "url": "/spotify-playlist-exporter/index.html"
+  },
+  {
+    "revision": "0bce8b64543188da0e4d",
+    "url": "/spotify-playlist-exporter/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "56555da92c068fe80372",
+    "url": "/spotify-playlist-exporter/static/css/main.75b1aa25.chunk.css"
+  },
+  {
+    "revision": "0bce8b64543188da0e4d",
+    "url": "/spotify-playlist-exporter/static/js/2.8df938cf.chunk.js"
+  },
+  {
+    "revision": "56555da92c068fe80372",
+    "url": "/spotify-playlist-exporter/static/js/main.7c04683f.chunk.js"
+  },
+  {
+    "revision": "ce85774247f9818ec1b2",
+    "url": "/spotify-playlist-exporter/static/js/runtime~main.92b1c832.js"
+  }
+]);
